93cd959ef889ecf4c4355f6151fed315510d1cf3
madiha303 <madiha303@gmail.com>
2025-03-14 10:28:52 +0100

    IconNav.jsx - login handling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
 <h3>Quisquam quam quas</h3>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam animi aspernatur, ipsum tempore repellat hic corrupti, quos sed tenetur, similique optio necessitatibus iste quis. Quisquam quam quas architecto alias veniam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum impedit aliquid iusto eaque, magni iste ipsa laboriosam voluptates. Voluptatum ipsam vitae laudantium dignissimos quis repellat dolorum reprehenderit magni voluptate quo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum minus non ab officia! Adipisci asperiores fuga ipsum excepturi voluptate itaque assumenda mollitia, odit iste voluptatem perspiciatis, numquam, voluptatibus iure quidem.</p>
 <h3>Quisquam quam quas</h3>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam animi aspernatur, ipsum tempore repellat hic corrupti, quos sed tenetur, similique optio necessitatibus iste quis. Quisquam quam quas architecto alias veniam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum impedit aliquid iusto eaque, magni iste ipsa laboriosam voluptates. Voluptatum ipsam vitae laudantium dignissimos quis repellat dolorum reprehenderit magni voluptate quo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum minus non ab officia! Adipisci asperiores fuga ipsum excepturi voluptate itaque assumenda mollitia, odit iste voluptatem perspiciatis, numquam, voluptatibus iure quidem.</p>
+
     </div>
 
 
